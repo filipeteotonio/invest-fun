7fbec6fac12dd89711045b60220e640dd918680a
Filipe Mendonça <filipetrm@gmail.com>
2018-08-12 19:30:26 -0300

    init of evaluation

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,8 +8,8 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon: 'person', class: '' },
+    { path: '/dashboard', title: 'Visão Geral',  icon: 'assignment', class: '' },
+    { path: '/user-profile', title: 'Avaliação',  icon: 'done_all', class: '' },
     { path: '/table-list', title: 'Table List',  icon: 'content_paste', class: '' },
     { path: '/typography', title: 'Typography',  icon: 'library_books', class: '' },
     { path: '/icons', title: 'Icons',  icon: 'bubble_chart', class: '' },
